247c2ffed2d71cf127721611c07d5eb32e723bd6
KennyXiaohuiYu <xhyu1969@hotmail.com>
2015-04-14 15:50:30 -0500

    commit test

---
+++ BTreeSearchParent/BTreeSearchParent/BNode.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace BTreeSearchParent
+{
+    public class BNode
+    {
+        public int BValue { get; set; }
+        public BNode LeftNode { get; set; }
+        public BNode RightNode { get; set; }
+
+        public BNode(int node)
+        {
+            BValue = node;
+        }
+
+        public static BNode ParentFinder(BNode head, BNode leaf1, BNode leaf2)
+        {
+            Stack<BNode> sLeaf1 = new Stack<BNode>();
+            Stack<BNode> sLeaf2 = new Stack<BNode>();
+            if (!FoundLeaf(head, leaf1, ref sLeaf1) || !FoundLeaf(head, leaf2, ref sLeaf2))
+                return null;
+
+            Stack<BNode> sLeaf1Reverse = ReverseStack(sLeaf1);
+            Stack<BNode> sLeaf2Reverse = ReverseStack(sLeaf2);
+            BNode prePopped = null;
+            bool bFound = false;
+            while (sLeaf1Reverse.Any() && sLeaf2Reverse.Any())
+            {
+                BNode leaf1Parent = sLeaf1Reverse.Pop();
+                BNode leaf2Parent = sLeaf2Reverse.Pop();
+                if (leaf1Parent != leaf2Parent)
+                {
+                    bFound = true;
+                    break;
+                }                
+                prePopped = leaf1Parent;
+            }
+            if (bFound)
+                return prePopped;
+
+            return null;
+        }
+
+
+        public static bool FoundLeaf(BNode head, BNode leaf, ref Stack<BNode> stack)
+        {
+            if (head == null)
+                return false;
+
+            stack.Push(head);
+            BNode curNode = head;
+
+            if (curNode.BValue == leaf.BValue)
+                return true;
+            
+            if (FoundLeaf(curNode.LeftNode, leaf, ref stack))
+                return true;
+
+            if (FoundLeaf(curNode.RightNode, leaf, ref stack))
+                return true;
+
+            stack.Pop();
+            return false;
+        }
+
+        public static Stack<BNode> ReverseStack(Stack<BNode> stack)
+        {
+            Stack<BNode> sReverse = new Stack<BNode>();
+            while (stack.Any())
+                sReverse.Push(stack.Pop());
+            return sReverse;
+        }
+    }
+}
---
+++ BTreeSearchParent/BTreeSearchParent/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTreeSearchParent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BNode node1 = new BNode(1);
+            BNode node2 = new BNode(2);
+            BNode node3 = new BNode(3);
+            BNode node4 = new BNode(4);
+            BNode node5 = new BNode(5);
+            BNode node6 = new BNode(6);
+            BNode node7 = new BNode(7);
+            node1.LeftNode = node2;
+            node1.RightNode = node3;
+            node3.LeftNode = node4;
+            node3.RightNode = node5;
+            node5.LeftNode = node6;
+            node5.RightNode = node7;
+
+            BNode node8 = new BNode(8);
+            BNode node9 = new BNode(9);
+            
+            BNode node = BNode.ParentFinder(node1, node4, node9);
+            PrintNode(node1, 0);
+            Console.WriteLine("Nearest Parent for Node {0} & {1} is: {2}", node4.BValue, node7.BValue, node.BValue);
+
+        }
+
+        private static void PrintNode(BNode node, int padding)
+        {
+            if (node == null)
+            {
+                for (int i = 0; i < padding; i++)
+                    Console.Write("\t");
+                Console.WriteLine('*');
+            }
+            else
+            {
+                PrintNode(node.RightNode, padding + 1);
+                for (int i = 0; i < padding; i++)
+                    Console.Write("\t");
+                Console.WriteLine(node.BValue);
+                PrintNode(node.LeftNode, padding + 1);
+            }
+        }        
+    }
+}
